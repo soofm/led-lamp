755a41a45cf4bef66b55438c73f475773437aead
Victor Lee <victorlee228@gmail.com>
2021-12-20 21:03:50 -0500

    Update LED lighting function

--- src/Lamp.ino
+++ src/Lamp.ino
@@ -2,49 +2,31 @@
 
 #define NUM_LEDS 29
 #define DATA_PIN 3
-
-DEFINE_GRADIENT_PALETTE( blue_gp ) {
-  0, 0, 39, 144,
-  85, 78, 188, 255,
-  170, 41, 114, 182,
-  255, 0, 39, 144
-};
-
-DEFINE_GRADIENT_PALETTE( purple_gp ) {  
-  0, 0, 39, 144,
-  85, 0, 29, 79,
-  170, 148, 92, 180,
-  255, 0, 39, 144
-};
+#define MAX_POWER_MILLIAMPS 500
+#define LED_TYPE WS2812B
+#define COLOR_ORDER GRB
 
 CRGB leds[NUM_LEDS];
 uint8_t whichPalette = 0;
-uint8_t offset;
-CRGBPalette16 currentPalette(blue_gp);
-CRGBPalette16 targetPalette(purple_gp);
+uint8_t offset = 0;
+CRGBPalette16 purple_palette = { 0x000000, 0x000000, 0x000000, 0x000000, 0x000000, 0x000000, 0x00008B, 0x00008B,
+                                 0x808018, 0x808078, 0x720752, 0x80095B, 0x980084, 0x980084, 0x980084, 0x9C0090 };
 
-void setup() { 
-  FastLED.addLeds<WS2812B, DATA_PIN, GRB>(leds, NUM_LEDS);
-  FastLED.setBrightness(50);
-  FastLED.setMaxPowerInVoltsAndMilliamps(5, 1000);
-  offset = 0;
+void setup() {
+  delay(3000);
+  FastLED.addLeds<LED_TYPE, DATA_PIN, COLOR_ORDER>(leds, NUM_LEDS)
+    .setCorrection(TypicalLEDStrip);
+  FastLED.setBrightness(128);
+  FastLED.setMaxPowerInVoltsAndMilliamps(5, MAX_POWER_MILLIAMPS);
 }
 
 void loop() {
+  fill_solid(leds, NUM_LEDS, CRGB(2, 2, 10));
+  
   for (int i = 0; i < NUM_LEDS; i++) {
-    leds[i] = ColorFromPalette(currentPalette, (offset + i * 2) % 256);
-  }
-
-  nblendPaletteTowardPalette(currentPalette, targetPalette, 10);
-
-  targetPalette = (whichPalette == 0) ? purple_gp : blue_gp;
-
-  EVERY_N_SECONDS(5) {
-    whichPalette = 1 - whichPalette;
-  }
-
-  EVERY_N_MILLISECONDS(5) {
-    offset = (offset + 1) % 256;
+    uint8_t index = inoise8(i * 24, millis() / 10);
+    CRGB color = ColorFromPalette(purple_palette, index, 96, LINEARBLEND);
+    leds[i] += color;
   }
 
   FastLED.show();
